0a8efe2ff65f1110d7acb6aba52a8fa6b6ff4df1
Library Guest <guest07@hhc.gov.ae>
2025-08-20 15:59:53 +0400

    added screenshot and photograph

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -9,18 +9,18 @@ function Main() {
     <div className="container">
       <div className="about-section">
         <div className="image-wrapper">
-          <img src="https://my-aws-assets.s3.us-west-2.amazonaws.com/portfolio-img/avatar_circle.jpeg" alt="Avatar" />
+          <img src="src/assets/images/" alt="Avatar" />
         </div>
         <div className="content">
           <div className="social_icons">
-            <a href="https://github.com/syedkaiser123" target="_blank" rel="noreferrer"><GitHubIcon/></a>
+            <a href="https://github.com/syedkaiser123?tab=repositories" target="_blank" rel="noreferrer"><GitHubIcon/></a>
             <a href="https://www.linkedin.com/in/syed-kaiser-mehmood-0316b9b5/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
           </div>
           <h1>Syed Kaiser</h1>
           <p>Senior Software Engineer</p>
 
           <div className="mobile_social_icons">
-            <a href="https://github.com/syedkaiser123" target="_blank" rel="noreferrer"><GitHubIcon/></a>
+            <a href="https://github.com/syedkaiser123?tab=repositories" target="_blank" rel="noreferrer"><GitHubIcon/></a>
             <a href="https://www.linkedin.com/in/syed-kaiser-mehmood-0316b9b5/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
           </div>
         </div>
