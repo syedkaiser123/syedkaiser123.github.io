0cf9aa4fe0e0cda71e2652852705b589ef2b19fb
Syed Kaiser <44358004+syedkaiser123@users.noreply.github.com>
2025-08-20 12:45:32 +0000

    changed photograph

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -9,7 +9,7 @@ function Main() {
     <div className="container">
       <div className="about-section">
         <div className="image-wrapper">
-          <img src="src/assets/images/" alt="Avatar" />
+          <img src="/src/assets/images/" alt="Avatar" />
         </div>
         <div className="content">
           <div className="social_icons">
